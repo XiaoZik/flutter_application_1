650b76be7c159f5757033f8277fa807e194ce50c
XiaoZik <zshafie@gmail.com>
2021-05-10 23:40:30 +0800

    first commit

---
+++ lib/screens/home/brew_tile.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/brew.dart';
+
+class BrewTile extends StatelessWidget {
+  final Brew brew;
+  BrewTile({this.brew});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 15.0,
+            backgroundColor: Colors.brown[brew.strength],
+            backgroundImage: AssetImage('assets/coffee_icon.png'),
+          ),
+          title: Text(brew.name),
+          subtitle: Text('Takes ${brew.sugars} sugar(s)'),
+        ),
+      ),
+    );
+  }
+}
